236b9256c56915e2003c83182d58ac2ed184baf0
msharee1 <murtuza.shareef@ymail.com>
2015-12-15 23:34:33 -0500

    End of exercises

--- ruby_book/exercises/1.rb
+++ ruby_book/exercises/1.rb
@@ -96,4 +96,27 @@ end
 
 p contacts
 
+arr = ['snow', 'winter', 'ice', 'slippery', 'salted roads', 'white trees']
+arr.delete_if {|entry| entry.start_with?("s") }
+p arr
 
+
+a = ['white snow', 'winter wonderland', 'melting ice',
+     'slippery sidewalk', 'salted roads', 'white trees']
+
+words = a.map do |word|
+	word.split
+end
+
+words = words.flatten
+
+p words
+
+hash1 = {shoes: "nike", "hat" => "adidas", :hoodie => true}
+hash2 = {"hat" => "adidas", :shoes => "nike", hoodie: true}
+
+if hash1 == hash2
+  puts "These hashes are the same!"
+else
+  puts "These hashes are not the same!"
+end
